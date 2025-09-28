27e1e8f7fd8589a277690db87cc4cc715d4aeba2
hritik2004-cse <146111768+hritik2004-cse@users.noreply.github.com>
2025-09-28 14:05:26 +0530

    implemented responsiveness

--- src/components/HeroSec.jsx
+++ src/components/HeroSec.jsx
@@ -12,21 +12,21 @@ import {
 
 const HeroSec = () => {
     return (
-        <div>
-            <Card>
+        <div className='w-full h-[85vh] flex items-center justify-center'>
+            <Card className='h-auto w-[90%] md:w-[70%] lg:w-[30%] backdrop-blur bg-black/30 border-none'>
                 <CardHeader>
-                    <CardTitle className="">
+                    <CardTitle className="text-center text-2xl text-white capitalize font-bold">
                         vowel checker
                     </CardTitle>
-                    <CardDescription>
-                        Enter text to count vowels
+                    <CardDescription className='text-white/50 text-center text-lg'>
+                        Enter the text below to count vowels
                     </CardDescription>
                 </CardHeader>
                 <CardContent>
-                    <Textarea />
+                    <Textarea placeholder='enter text here ...' className='h-[30vh] text-white text-xl border-gray-500/30 placeholder:text-lg'/>
                 </CardContent>
-                <CardFooter>
-                    <Button>Count vowels</Button>
+                <CardFooter className='w-full'>
+                    <Button className='w-full bg-gradient-to-r h-[6vh] from-[#9363ff] via-[#c1a6ff] to-[#f5f0ff] text-lg capitalize font-[500]'>Count vowels</Button>
                 </CardFooter>
             </Card>
         </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,7 +7,7 @@ const Nav = () => {
         <nav className='w-full flex h-[10vh] items-center justify-between bg-transparent py-9'>
             <div className="w-[90%] mx-auto flex items-center justify-between">
             <h1 className="text-md sm:text-lg md:text-2xl lg:text-3xl font-bold text-white font-serif capitalize">vowel counter</h1>
-            <Button className='bg-gradient-to-r from-[#9363ff] via-[#c1a6ff] to-[#f5f0ff] rounded-3xl py-7 capitalize sm:text-sm md:text-md lg:text-lg hover:scale-[1.1] transition ease-in duration-400 font-bold'>
+            <Button className='bg-gradient-to-r from-[#9363ff] via-[#c1a6ff] to-[#f5f0ff] rounded-3xl py-7 px-7 capitalize sm:text-sm md:text-md lg:text-lg hover:scale-[1.1] transition ease-in duration-400 font-[500]'>
                 explore more <span className="hidden lg:block">projects</span>
                 <FaArrowRight />
             </Button>
